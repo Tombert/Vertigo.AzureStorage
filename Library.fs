4d507238806515eebe8eb6fbc96fb6ea3951e07c
Thomas Gebert <thomas@gebert.sexy>
2017-11-18 00:35:34 -0500

    Various fixes

--- Library.fs
+++ Library.fs
@@ -7,10 +7,29 @@ module AzureStorage =
     open Microsoft.WindowsAzure.Storage.Queue;
 
     module AzureQueue = 
+
+        type CloudQueue with  
+            member this.AsyncAddMessage(c: CloudQueueMessage) = 
+                this.AddMessageAsync c
+                |> Async.AwaitTask
+
+            member this.DeleteMessage(c: CloudQueueMessage) = 
+                c
+                |> this.DeleteMessageAsync 
+                |> Async.AwaitTask
+                |> Async.RunSynchronously
+
+            member this.AsyncDeleteMessage (c: CloudQueueMessage) =
+                c
+                |> this.DeleteMessageAsync
+                |> Async.AwaitTask
+
+
         let fromAccountStr acc topic = 
             let storageAccount = CloudStorageAccount.Parse(acc)
             let queueClient = storageAccount.CreateCloudQueueClient();
-            queueClient.GetQueueReference("messageQueue");
+            queueClient.GetQueueReference(topic);
+
         let toAsyncSeq (queue: CloudQueue) (chunkSize: int) (pollrate: System.TimeSpan) = asyncSeq {
             while true do
                 let! messages = queue.GetMessagesAsync(chunkSize) |> Async.AwaitTask
